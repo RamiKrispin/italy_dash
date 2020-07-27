9d5e95741991ecd2aaf941614c1f962d0b362d45
RamiKrispin <ramkrisp@umich.edu>
2020-07-26 17:46:49 -0700

    fix typo

--- bash/data_refresh.sh
+++ bash/data_refresh.sh
@@ -2,7 +2,7 @@
 
 echo "Rendering the dashboard..."
 
-Rscript -e "rmarkdown::render_site()""
+Rscript -e "rmarkdown::render_site()"
 
 if [[ "$(git status --porcelain)" != "" ]]; then
     git config --global user.name 'RamiKrispin'
