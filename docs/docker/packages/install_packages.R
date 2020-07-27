f8afd04766c1cd610ecf4b4f86759c7fd1d25045
RamiKrispin <ramkrisp@umich.edu>
2020-07-26 22:49:32 -0700

    update the dashboard

---
+++ docs/docker/packages/install_packages.R
@@ -0,0 +1,47 @@
+# installing package imports packages
+pkg_list <- c("dplyr",
+              "remotes",
+              "devtools",
+              "here",
+              "lubridate",
+              "magrittr",
+              "rmarkdown",
+              "tidyr",
+              "testthat",
+              "pkgdown",
+              "shiny",
+              "plotly",
+              "ggplot2",
+              "flexdashboard",
+              "knitr",
+              "usethis")
+for(i in pkg_list){
+  if(!i %in% rownames(installed.packages())){
+    install.packages(pkgs = i, repos = "https://cran.rstudio.com/")
+  }
+  
+  if(!i %in% rownames(installed.packages())){
+    stop(paste("Package", i, "is not available"))
+  }
+}
+
+
+# Install covid19 packages
+devtools::install_github("Covid19R/covid19italy")
+devtools::install_github("Covid19R/coronavirus")
+
+f <- FALSE
+for(i in c(pkg_list, "covid19italy", "coronavirus")){
+
+  cat(i, "...")
+  if(i %in% rownames(installed.packages())){
+    cat("OK\n")
+  } else{
+    cat("Fail\n")
+    f <- TRUE
+  }
+    if(f){
+    stop("One or more packages were missing")
+    }
+}
+
